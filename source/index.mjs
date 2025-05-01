ce11536bf7759c232427f94000a159d2f5c6d3c7
devConcordia <dev.rsconcordia@gmail.com>
2025-05-01 09:03:39 -0300

    restruct: move index.mjs to source/

---
+++ source/index.mjs
@@ -0,0 +1,22 @@
+
+import Vector2 from './math/Vector2.mjs';
+import Vector3 from './math/Vector3.mjs';
+import Vector4 from './math/Vector4.mjs';
+import Matrix3 from './math/Matrix3.mjs';
+import Matrix4 from './math/Matrix4.mjs';
+import Color from './math/Color.mjs';
+
+import Geometry3 from './geometry/Geometry3.mjs';
+
+import Renderer from './Renderer.mjs';
+
+export default {
+	Renderer,
+	Vector2,
+	Vector3,
+	Vector4,
+	Matrix3,
+	Matrix4,
+	Color,
+	Geometry3
+}
