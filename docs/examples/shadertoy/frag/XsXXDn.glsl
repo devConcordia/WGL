fc3cad80d336b8e544052d4d0f854111b505586a
devConcordia <dev.rsconcordia@gmail.com>
2025-09-04 18:17:33 -0300

    fix: corrige o fragment shader

--- docs/examples/shadertoy/frag/XsXXDn.glsl
+++ docs/examples/shadertoy/frag/XsXXDn.glsl
@@ -1,28 +1,35 @@
 
 precision highp float;
-
 uniform vec2      iResolution;           // viewport resolution (in pixels)
 uniform float     iTime;                 // shader playback time (in seconds)
 
-
-// http://www.pouet.net/prod.php?which=57245
-// If you intend to reuse this shader, please add credits to 'Danilo Guanabara'
-
-#define t iTime
-#define r iResolution.xy
-
 void main(){
+	
+	vec2 r = iResolution.xy;
 	vec3 c;
-	float l,z=t;
-	for(int i=0;i<32;i++) {
-		vec2 uv,p=gl_FragCoord.xy/r;
-		uv=p;
-		p-=.5;
-		p.x*=r.x/r.y;
-		z+=.07;
-		l=length(p);
-		uv+=p/l*(sin(z)+1.)*abs(sin(l*9.-z-z));
-		c[i]=.01/length(mod(uv,1.)-.5);
+	
+	float l,
+		z = iTime;
+	
+	for( int i = 0; i < 3; i++ ) {
+		
+		vec2 uv,
+		p = gl_FragCoord.xy/r;
+		
+		uv = p;
+		p -= .5;
+		p.x *= r.x/r.y;
+		
+		z += .07;
+		
+		l = length(p);
+		
+		uv += p/l * (sin(z)+1.) * abs(sin(l*9.-z-z));
+		
+		c[i] = .01/length(mod(uv,1.)-.5);
+		
 	}
-	gl_FragColor=vec4(c/l,t);
+	
+	gl_FragColor = vec4( c/l, iTime );
+	
 }
